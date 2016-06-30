99b5dea69026dd5c1161391be2c3650453e2beb2
CashCraigVarian <cash.craig@varian.com>
2016-06-30 15:00:30 -0500

    commit 1

--- problem-solving/Program.cs
+++ problem-solving/Program.cs
@@ -16,49 +16,49 @@ namespace problem_solving
         public static long SumArray(IEnumerable<int> arr)
         {
             // return the sum of all the values in the array
-            // TODO
-            return 0;
+
+            return arr.Sum(num => (long)num);
         }
 
         public static long SumArrayOddValues(IEnumerable<int> arr)
         {
             // return the sum of all the values in the array that are odd
-            // TODO
-            return 0;
+
+            return arr.Where(num => num % 2 != 0).Sum(num => (long)num);
         }
 
         public static long SumArrayEverySecondValue(IEnumerable<int> arr)
         {
             // return the sum of every second value in the array. i.e. the 2nd value + the 4th value + the 6th value ...
-            // TODO
-            return 0;
+
+            return arr.Where((num,index) => index % 2 != 0).Sum(num => (long)num);
         }
 
         public static IEnumerable<int> GetUniqueValues(IEnumerable<int> arr)
         {
             // return an array that contains only unique values from the passed in array
-            // TODO
-            return null;
+
+            return arr.Distinct();
         }
 
         public static IEnumerable<int> GetArrayIntersect(IEnumerable<int> arrA, IEnumerable<int> arrB)
         {
             // return an array that contains all the values that are in array A and array B
-            // TODO
-            return null;
+
+            return arrA.Intersect(arrB);
         }
 
         public static IEnumerable<int> GetArrayNotIntersect(IEnumerable<int> arrA, IEnumerable<int> arrB)
         {
             // return an array that contains all the values that are in array A or array B but not in both array A and array B
-            // TODO
-            return null;
+
+            return arrA.Concat(arrB).Except(arrA.Intersect(arrB));
         }
 
         public static Boolean HasSum(IEnumerable<int> arr, long target)
         {
             // return true if any 2 values in the array have a sum equal to the target value
-            // TODO
+
             return false;
         }
 
@@ -66,27 +66,28 @@ namespace problem_solving
         {
             // Given an array of int values, return their sum. 
             // However, if any of the values is the same as another of the values, it does not count towards the sum.
-            return 0;
+            return arr.Where(num1 => arr.Where(num2 => num1 == num2).Count() == 1 ).Sum(num => (long)num);
         }
 
         public static String DoubleString(String s)
         {
             // return a string that is the original string with each character in the string repeated twice
             // e.g. for input "ABCDE", return "AABBCCDDEE"
-            return null;
+            return s;
         }
 
         public static int CountChars(String s, char c)
         {
             // return the count of how many times char c occurs in string s
-            return 0;
+            return s.Count(c2 => c == c2);
         }
 
         public static long SumDigits(String s)
         {
             // return the sum of the digits 0-9 that appear in the string, ignoring all other characters
             // e.g. "123" return 6
-            return 0;
+            short result;
+            return s.Where(c => Int16.TryParse(c.ToString(), out result)).Sum(num => Int16.Parse(num.ToString()));
         }
 
         public static long SumNumbers(String s)
@@ -94,7 +95,7 @@ namespace problem_solving
             // return the sum of the numbers that appear in the string, ignoring all other characters
             // a number is a series of 1 or more digits in a row
             // e.g. "11 22" returns 33
-            return 0;
+            return s.Sum(num => Int16.Parse(num.ToString()));
         }
 
         public static Boolean IsAnagram(String s1, String s2)
@@ -111,7 +112,28 @@ namespace problem_solving
             // Given 2 integer values greater than 0, 
             // return whichever value is nearest to 21 without going over. 
             // Return 0 if they both go over.
-            return 0;
+
+            int winner = 0;
+            int[] counts = {count1, count2};
+
+            foreach (int i in counts)
+            {
+                if (i <= 21 && i > winner)
+                    winner = i;
+            }
+            return winner;
+
+            //ALTERNATE METHOD
+
+            //return count1 > 21 
+            //        ? count2 > 21 
+            //            ? 0 
+            //            : count2 
+            //        : count2 > 21 
+            //            ? count1 
+            //            : count1 > count2 
+            //                ? count1 
+            //                : count2;
         }
 
         public static int FivePlayerBlackJack(int count1, int count2, int count3, int count4, int count5)
@@ -119,7 +141,16 @@ namespace problem_solving
             // Given 5 integer values greater than 0, 
             // return whichever value is nearest to 21 without going over. 
             // Return 0 if they all go over.
-            return 0;
+
+            int winner = 0;
+            int[] counts = { count1, count2, count3, count4, count5 };
+
+            foreach (int i in counts)
+            {
+                if (i <= 21 && i > winner)
+                    winner = i;
+            }
+            return winner;
         }
 
         public static int NPlayerBlackJack(IEnumerable<int> counts)
@@ -127,7 +158,17 @@ namespace problem_solving
             // Given a list of integer values greater than 0, 
             // return whichever value is nearest to 21 without going over. 
             // Return 0 if they all go over.
-            return 0;
+            if(counts == null)
+                throw new ArgumentNullException();
+
+            int winner = 0;
+
+            foreach(int i in counts)
+            {
+                if(i <= 21 && i > winner)
+                    winner = i;
+            }
+            return winner;
         }
 
         public static Dictionary<String, int> WordCount(IEnumerable<String> arr)
